ed3694e7549810d8c487ea5134c8d8d50b83025d
christinawagner <cwagner0604@gmail.com>
2017-08-17 17:23:15 -0400

    Initial commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using cwagnerShoppingApp.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace cwagnerShoppingApp.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(context));
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            if (!context.Users.Any(u => u.Email == "cwagner0604@gmail.com"))
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "cwagner0604@gmail.com",
+                    Email = "cwagner0604@gmail.com",
+                    FirstName = "Christina",
+                    LastName = "Wagner",
+                }, "Password1!");
+            }
+            var userId = userManager.FindByEmail("cwagner0604@gmail.com").Id;
+            userManager.AddToRole(userId, "Admin");
+        }
+    }
+}
---
+++ assets/ecommerce.js
@@ -0,0 +1,25 @@
+﻿function NavToggle() {
+    var changeDisplay = document.getElementById("navBar");
+    if (changeDisplay.style.display == "none")
+    { changeDisplay.style.display = "block"; }
+    else
+    { changeDisplay.style.display = "none"; }
+}
+function yearToggle() {
+    var changeYear = document.getElementById("years");
+    if (changeYear.style.display == "none")
+    {
+        changeYear.style.display = "block";
+        document.getElementById("navIcon").classList.remove("fa-plus-square");
+        document.getElementById("navIcon").classList.add("fa-minus-square");
+    }
+    else {
+        changeYear.style.display = "none";
+        document.getElementById("navIcon").classList.remove("fa-minus-square");
+        document.getElementById("navIcon").classList.add("fa-plus-square");
+    }
+}
+//shopping cart hover
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
