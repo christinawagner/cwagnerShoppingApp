93d1474586f171d5b4452612d34f8cea9b557747
christinawagner <cwagner0604@gmail.com>
2017-08-21 21:22:32 -0400

    added TinyMCE

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -167,7 +167,7 @@ namespace cwagnerShoppingApp.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Index");
+                    return RedirectToAction("Index", "Items");
                 }
                 AddErrors(result);
             }
--- Models/CodeFirst/Item.cs
+++ Models/CodeFirst/Item.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace cwagnerShoppingApp.Models.CodeFirst
 {
@@ -15,6 +16,7 @@ namespace cwagnerShoppingApp.Models.CodeFirst
         public decimal Price { get; set; }
         [Required]
         public string MediaURL { get; set; }
+        [AllowHtml]
         public string Description { get; set; }
     }
 }
